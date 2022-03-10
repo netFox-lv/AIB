7a492b7a1d4d91ae1b2f6435b32e31091b6c3a6a
netFox <arturs.isaks@outlook.lv>
2022-03-10 18:19:32 +0200

    frontend hotfix

--- AIB/AIB/settings.py
+++ AIB/AIB/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework',
-    'frontend'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- AIB/frontend/urls.py
+++
@@ -1,7 +0,0 @@
-import imp
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-]
