6aa9eed40d0f1dac5cdc8fe044b4f86ebfe2ed67
KrishyB <laciskrisjanis0@gmail.com>
2022-03-12 18:59:51 +0200

    Added a view for the API to send JSON data, i think

--- AIB/api/views.py
+++ AIB/api/views.py
@@ -1,3 +1,17 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.parsers import JSONParser
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+def AddAgreement(request):
+    
+    if request.method == 'POST':#Add agreement using JSON through POST method
+        data = JSONParser().parse(request)#from JSON
+        serializer = AgreementSerializer(data=data)#into table
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)#failed to reckognize data 
