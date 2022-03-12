bef717aa582c6e4ecc12b8ab6871b7049c0addf0
KrishyB <laciskrisjanis0@gmail.com>
2022-03-12 15:17:30 +0200

    made agreement model, made Agreement serializer, made admin account lacis

--- AIB/api/models.py
+++ AIB/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Agreement(models.Model):
+    id = models.AutoField(primary_key=True)
+    agreement_numurs = models.IntegerField()
+    amount = models.FloatField()
+    due_to_date = models.DateField()
+
+    def __str__(self):
+        return self.name
