e7c36f66b74894dde7e1a850460fca3c78ace6bc
KrishyB <laciskrisjanis0@gmail.com>
2022-03-12 20:32:11 +0200

    added path for POST

--- AIB/AIB/urls.py
+++ AIB/AIB/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
-
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('AddAgreement/',views.AddAgreement),
     #path('',include('frontend.urls')),
 ]
--- AIB/api/views.py
+++ AIB/api/views.py
@@ -1,16 +1,18 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import *
 from .serializers import *
 
 # Create your views here.
 
+@csrf_exempt #ignore if csrf token is not set(unsafe)
 def AddAgreement(request):
-    
+
     if request.method == 'POST':#Add agreement using JSON through POST method
         data = JSONParser().parse(request)#from JSON
-        serializer = AgreementSerializer(data=data)#into table
+        serializer = AgreementSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=201)
