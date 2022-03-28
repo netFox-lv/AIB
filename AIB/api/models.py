b2036e907d664cdc36e46389cd232efe79509b6f
KrishyB <laciskrisjanis0@gmail.com>
2022-03-28 19:42:30 +0300

    Invoice api

--- AIB/api/models.py
+++ AIB/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from api.validators import val_period
 from api.validators import val_num
 # Create your models here.
 
@@ -8,3 +9,14 @@ class Agreement(models.Model):
     amount = models.FloatField()
     due_to_date = models.DateField()
     #access_path = models.FilePathField(path="/agreements")# file_path -> i need download -> get a link
+
+class Invoice(models.Model):
+    id = models.IntegerField(primary_key=True)
+    invoice_number = models.TextField(unique=True,validators=[val_num])
+    amount = models.FloatField()
+    invoice_date = models.DateField()
+    payment_to_date = models.DateField()
+    payment_period = models.TextField(validators=[val_period])
+    #payment_period_month
+    #payment_period_year
+    #paid: bool
