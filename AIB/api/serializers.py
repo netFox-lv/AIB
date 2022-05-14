e23589b1d72d3427fd52b71e630b3f59d2cc6b66
netFox <arturs.isaks@outlook.lv>
2022-05-14 18:59:45 +0300

    hotfix-aib-25

--- AIB/api/serializers.py
+++ AIB/api/serializers.py
@@ -2,7 +2,7 @@ from dataclasses import fields
 from api.models import Invoice
 from rest_framework import serializers
 
-from api.models import Agreement
+from api.models import Agreement, UploadPdf
 
 class AgreementSerializer(serializers.ModelSerializer):
     class Meta:
@@ -31,4 +31,12 @@ class InvoiceSerializer(serializers.ModelSerializer):
             'payment_period_month',
             'payment_period_year',
             'paid',
+        )
+        
+class PdfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UploadPdf
+        fields = (
+            'name',
+            'pdf',
         )
